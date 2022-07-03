4fa2fb1d014bdb5089767743b24dea0e75786550
Raghav <ragsaxen@cisco.com>
2022-07-03 18:28:12 +0530

    Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,33 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("This is First Code"));
+    var questions = [
+      "What's your favorite Color?",
+      "What's your favorite DIsh?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First Quiz App"),
+        ),
+        body: Column(
+          children: [
+            Text("The Questions"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
