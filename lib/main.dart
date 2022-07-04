b5c8cc158aa947247e03e08263740db6d259aa1f
Raghav <ragsaxen@cisco.com>
2022-07-04 16:44:51 +0530

    Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -14,44 +14,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': "What's your favorite Color?",
+      'answers': ["Red", "Blue", "Green", "Black", "Yellow"]
+    },
+    {
+      'questionText': "What's your favorite Dish?",
+      'answers': ["Puri", "Chicken", "Wraps", "Rice Bowl", "Noodles"]
+    },
+    {
+      'questionText': "What's your favorite Animal?",
+      'answers': ["Dog", "Cat", "Elephant", "Tiger", "Lion"]
+    }
+  ];
+
   var _questionIndex = 0;
 
   void _answerChose() {
-    setState(() {
-      _questionIndex++;
-    });
-    print("Answer Chosen");
+    if (_questionIndex < _questions.length) {
+      setState(() {
+        _questionIndex++;
+      });
+      print("Answer Chosen");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var _questions = [
-      {
-        'questionText': "What's your favorite Color?",
-        'answers': ["Red", "Blue", "Green", "Black", "Yellow"]
-      },
-      {
-        'questionText': "What's your favorite Dish?",
-        'answers': ["Puri", "Chicken", "Wraps", "Rice Bowl", "Noodles"]
-      },
-      {
-        'questionText': "What's your favorite Animal?",
-        'answers': ["Dog", "Cat", "Elephant", "Tiger", "Lion"]
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Quiz App"),
         ),
-        body: Column(
-          children: [
-            Question(_questions[_questionIndex]['questionText']),
-            ...(_questions[_questionIndex]['answers'] as List<String>)
-                .map((e) => Answers(_answerChose, e))
-                .toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Column(
+                children: [
+                  Question(_questions[_questionIndex]['questionText']),
+                  ...(_questions[_questionIndex]['answers'] as List<String>)
+                      .map((e) => Answers(_answerChose, e))
+                      .toList()
+                ],
+              )
+            : Center(child: Text("You have Completed the Quiz")),
       ),
     );
   }
